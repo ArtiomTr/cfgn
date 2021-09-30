1addd4dad0b97cb526a0b70cc3bebec4cd463b39
sirse <artiom.tretjakovas2@gmail.com>
2021-09-30 22:27:41 +0300

    Fixed build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './parseConfiguration';
 export * from './parseJsConfiguration';
 export * from './parseJsonConfiguration';
+export * from './parseEsmConfiguration';
+export * from './Format';
+export * from './Parser';
