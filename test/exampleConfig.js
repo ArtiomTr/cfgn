c90797f752d198ae8fbacfb83114103944aa3c29
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-12-24 01:08:15 +0200

    Fixed tests

--- test/exampleConfig.js
+++
@@ -1,6 +0,0 @@
-const { resolve } = require('path');
-
-module.exports = {
-    someConfig: 'hello',
-    t: resolve(__dirname, 'path.ts'),
-};
--- test/parseJsConfiguration.test.ts
+++ test/parseJsConfiguration.test.ts
@@ -1,9 +1,21 @@
 import { parseJsConfiguration } from '../src/parseJsConfiguration';
 import { resolve } from 'path';
+import { readFile } from 'fs-extra';
+import { mocked } from 'ts-jest/utils';
 
 describe('parseJsConfiguration', () => {
     it('should require js file', async () => {
-        expect(await parseJsConfiguration(resolve(__dirname, 'exampleConfig.js'))).toStrictEqual({
+        mocked(readFile).mockImplementationOnce(async () =>
+            Buffer.from(`
+const { resolve } = require('path');
+
+module.exports = {
+    someConfig: 'hello',
+    t: resolve(__dirname, 'path.ts'),
+};
+`),
+        );
+        expect(await parseJsConfiguration(resolve(__dirname, 'exampleConfig.js'))).toEqual({
             someConfig: 'hello',
             t: resolve(__dirname, 'path.ts'),
         });
