2b4ccf4cee2bb9594df251e75ac1d478946da089
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-12-24 01:18:51 +0200

    Bug fix

--- src/parseConfiguration.ts
+++ src/parseConfiguration.ts
@@ -2,23 +2,22 @@ import { extname } from 'path';
 
 import { Format } from './Format';
 import { parseEsmConfiguration } from './parseEsmConfiguration';
+import { parseJsConfiguration } from './parseJsConfiguration';
 import { parseJsonConfiguration } from './parseJsonConfiguration';
 import { Parser } from './Parser';
 import { parseTsConfiguration } from './parseTsConfiguration';
-import { parseJsConfiguration } from '.';
 
-const parsers: Record<Format, () => Parser> = {
-    [Format.JSON]: () => parseJsonConfiguration,
-    [Format.JS]: () => parseJsConfiguration,
-    [Format.COMMONJS]: () => parseJsConfiguration,
-    [Format.ESMODULE]: () => parseEsmConfiguration,
-    [Format.TS]: () => parseTsConfiguration,
+const parsers: Record<Format, Parser> = {
+    [Format.JSON]: parseJsonConfiguration,
+    [Format.JS]: parseJsConfiguration,
+    [Format.COMMONJS]: parseJsConfiguration,
+    [Format.ESMODULE]: parseEsmConfiguration,
+    [Format.TS]: parseTsConfiguration,
 };
 
 export const parseConfiguration = async (file: string, format: Format = extname(file) as Format) => {
     if (format in parsers) {
-        const currentParser = parsers[format]();
-        return currentParser(file);
+        return parsers[format](file);
     }
 
     throw new Error('Unknown format');
